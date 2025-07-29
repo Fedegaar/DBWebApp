18c5cca43a3b60783fa00dd7b448be2b00fd69cf
Federico G. <fedegaar@gmail.com>
2025-07-28 22:54:56 -0300

    Primer commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Soy el header</h1>
+      <h2>Bienvenidos a este nuevo espacio</h2>
+    </div>
+  )
+}
+
+export default Header
